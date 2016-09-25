20b96e78e469d67329b08b1539adb41241a1d4be
Datta <dattakadiyala@gmail.com>
2016-09-24 21:55:30 -0500

    Initial commit with map view

--- Hot Lanes/ViewController.swift
+++ Hot Lanes/ViewController.swift
@@ -7,18 +7,24 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mapView: MKMapView!
+    let regionRadius: CLLocationDistance = 12000
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let initialLocation = CLLocation(latitude: 29.760427, longitude: -95.369803)
+        
+        centerMapOnLocation(location: initialLocation)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                                  regionRadius * 10.0, regionRadius * 10.0)
+        mapView.setRegion(coordinateRegion, animated: true)
     }
+  
 
 
 }
